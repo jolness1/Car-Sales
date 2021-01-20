25055f17e8c0366fc3357a48e0e80e8289be98f7
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-01-19 18:39:12 -0700

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+
+
 const AddedFeature = props => {
+  const handleRemove = (e) => {
+    props.removeFeature(props.feature);
+  } 
+  
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={handleRemove}>X</button>
       {props.feature.name}
     </li>
   );
